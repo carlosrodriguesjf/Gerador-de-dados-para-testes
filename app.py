fc85e42f2018aac3d894f65c9bc43a1debdbb844
Carlos Rodrigues <carlosrodriguesjf@gmail.com>
2023-04-27 21:45:56 -0300

    Adicionado o loop e a lógica para parada e iniciando a lógoca de gravar em um arquivo

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 # Projeto 1 - Gerador de dados para testes(app no terminal)
 
 # Importação de bibliotecas
-
 import random
-
-
 
 
 # Criar uma lista de 5 nomes, 5 e-mail, 5 telefones, 5 cidades e 5 estados
@@ -16,49 +13,97 @@ estado =['MG','RJ','CE','PR','SC']
 
 
 # Criação das variáveis do programa
-
+selecao_do_usuario = 0
+salvar_em_arquivo = 0
 
 # Criando funções para cada ação que será escolhida
 
 def selecao1():
-    nome_aleatorio = random.choice(nomes)
-    return nome_aleatorio
+    return random.choice(nomes)
+    print('')
+    print(100*'-')
 
 def selecao2():
-    email_aleatorio = random.choice(emails)
+    return random.choice(emails)
+    print('')
+    print(100*'-')
 
 def selecao3():
-    telefone_aleatorio = random.choice(telefone)
+    return random.choice(telefone)
+    print('')
+    print(100*'-')
 
 def selecao4():
-   cidade_aleatoria =  random.choice(cidade)
+    return random.choice(cidade)
+    print('')
+    print(100*'-')
 
 def selecao5():
-    estado_aleatorio = random.choice(estado)
+    return random.choice(estado)
+    print('')
+    print(100*'-')
+
+def cria_arquivo(dado):
+    with open('dados.txt','w',newline='',encoding='utf-8') as arquivo:
+        arquivo.write(dado)
 
 
 
-
-
+while True:
 # Criação o menu inicial
-print('Bem-vindo ao Gerador de Dados de Testes - Para finalizar o programa, digite "parar"')
-print(30*'-')
-print('Escolhe uma ou mais opções abaixo a serem geradas aleatóriamente')
-print('[1] - Nome')
-print('[2] - E-mail')
-print('[3] - Telefone')
-print('[4] - Cidade')
-print('[5] - Estado')
-selecoes_do_usuario = input('Digite uma(as) opções: ')
+    print('Bem-vindo ao Gerador de Dados de Testes - Para finalizar o programa, digite "parar"')
+    print(100*'-')
+    print('Escolha uma ou mais opções abaixo a serem geradas aleatóriamente')
+    print('[1] - Nome')
+    print('[2] - E-mail')
+    print('[3] - Telefone')
+    print('[4] - Cidade')
+    print('[5] - Estado')
+    selecao_do_usuario = input('Digite uma(as) opções: ')
+    print(100*'-')
+
+    if selecao_do_usuario == 'parar':
+        break
+
+    # Criar lógica para salvar os dados em um arquivo de texto
+    salvar_em_arquivo = input('Gostaria de salvar os dados em um arquivo de texto? (s/n) ')
+    if salvar_em_arquivo == 's':
+        print('Arquivo salvo')
+
+    print('')
+    print('')
+
+    
+    # Criando a lógica para exibir o nome aleatorio
+    if selecao_do_usuario == '1':
+        nome = selecao1()
+        print(nome)
+        cria_arquivo(nome)
+    if selecao_do_usuario == '2':
+        email = selecao2()
+        print(email)
+    if selecao_do_usuario == '3':
+        telefone = selecao3()
+        print(telefone)
+    if selecao_do_usuario == '4':
+        cidade = selecao4()
+        print(cidade)
+    if selecao_do_usuario == '5':
+        estado = selecao5()
+        print(estado)
+    
 
 
-# Criando a lógica para exibir o nome aleatorio
+print('Encerrando o programa')
+print('')
 
 
 
 
 
-# Criar lógica para salvar os dados em um arquivo de texto
+
+
+
 
 
 
